4e181e9e1a8cdee61497c722581bfa7038cf9c2a
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2021-11-13 11:37:52 +0530

    Exception completed

--- exception_handling/bin/exception_handling.dart
+++ exception_handling/bin/exception_handling.dart
@@ -1,3 +1,26 @@
+//Exception class
+
+class InvalidPhoneNumberExceptionClass implements Exception {}
+
+bool? validatePhoneNumber(String phonenumber) {
+  //checking phonenumber length
+  if (phonenumber.length == 10) {
+    return true;
+  } else {
+    throw InvalidPhoneNumberExceptionClass(); // throwing exception
+  }
+}
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Exception handling ');
+  //handling the exception
+  try {
+    final phoneValid = validatePhoneNumber('9496');
+  } on InvalidPhoneNumberExceptionClass catch (_) {
+    // specifiying exception and handling
+    print('invalid phone number exception : $_');
+  } catch (e) {
+    // genaral catch block
+    print("Exception : ${e}");
+  }
 }
